ddd6fb0060a0c626f5469919cb27f63e3eab9cdf
Pedro Lucas <pedrolucas1014zz@gmail.com>
2022-05-16 08:35:40 -0300

    Adding Ctrl and shift key

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -322,6 +322,8 @@ class DropBoxController {
           <div class="name text-center">${file.originalFilename}</div>
         `;
 
+    this.initEventsLi(li);
+
     return li;
   }
 
@@ -340,7 +342,60 @@ class DropBoxController {
       });
     });
   }
+
+  initEventsLi(li) {
+
+    li.addEventListener('click', e => {
+
+      if (e.shiftKey) {
+
+        let firstLi = this.listFilesEl.querySelector('.selected');
+
+        if (firstLi) {
+
+          let indexStart;
+          let indexEnd;
+          let lis = li.parentElement.childNodes
+
+          lis.forEach((el, index) => {
+
+            if (firstLi === el) indexStart = index;
+            if (li === el) indexEnd = index;
+          });
+
+          let index = [indexStart, indexEnd].sort();
+
+
+          lis.forEach((el, i) => {
+            if (i >= index[0] && i <= index[1]) {
+              el.classList.add('selected');
+            }
+
+          });
+
+          return true;
+        }
+      }
+
+
+      // Ctrl
+      if (!e.ctrlKey) {
+
+        this.listFilesEl.querySelectorAll('li.selected').forEach(el => {
+
+          el.classList.remove('selected');
+
+
+        });
+
+      }
+
+      li.classList.toggle('selected')
+
+    })
+
+  }
+
 }
 
 
-// FINALIZAR ATENRANDO SVG PARA APARECER NA TELA 
